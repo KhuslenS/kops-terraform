911cd8183786916cdc3fb83fa8d8b4c7781fd3e0
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-17 12:13:18 -0500

    updated

--- kubernetes.tf
+++ kubernetes.tf
@@ -1056,7 +1056,3 @@ resource "aws_vpc_dhcp_options_association" "khuslenkubernetes-com" {
   vpc_id          = "${aws_vpc.khuslenkubernetes-com.id}"
   dhcp_options_id = "${aws_vpc_dhcp_options.khuslenkubernetes-com.id}"
 }
-
-terraform = {
-  required_version = ">= 0.9.3"
-}
