ba9dd6292eff829ad97a3ce91a8737c2d94f963b
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-17 12:18:35 -0500

    updated

---
+++ bastion-asg.tf
@@ -0,0 +1,34 @@
+resource "aws_autoscaling_group" "bastions-khuslenkubernetes-com" {
+  name                 = "bastions.khuslenkubernetes.com"
+  launch_configuration = "${aws_launch_configuration.bastions-khuslenkubernetes-com.id}"
+  max_size             = 1
+  min_size             = 1
+  vpc_zone_identifier  = ["${aws_subnet.utility-us-west-2a-khuslenkubernetes-com.id}", "${aws_subnet.utility-us-west-2b-khuslenkubernetes-com.id}", "${aws_subnet.utility-us-west-2c-khuslenkubernetes-com.id}"]
+
+  tag = {
+    key                 = "KubernetesCluster"
+    value               = "khuslenkubernetes.com"
+    propagate_at_launch = true
+  }
+
+  tag = {
+    key                 = "Name"
+    value               = "bastions.khuslenkubernetes.com"
+    propagate_at_launch = true
+  }
+
+  tag = {
+    key                 = "k8s.io/cluster-autoscaler/node-template/label/kops.k8s.io/instancegroup"
+    value               = "bastions"
+    propagate_at_launch = true
+  }
+
+  tag = {
+    key                 = "k8s.io/role/bastion"
+    value               = "1"
+    propagate_at_launch = true
+  }
+
+  metrics_granularity = "1Minute"
+  enabled_metrics     = ["GroupDesiredCapacity", "GroupInServiceInstances", "GroupMaxSize", "GroupMinSize", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
+}
