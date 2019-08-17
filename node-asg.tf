050d886efd2e92fc7378250f9a9bd44ece92b1fa
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-17 13:12:17 -0500

    updated

--- node-asg.tf
+++ node-asg.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_group" "nodes-khuslenkubernetes-com" {
   name                 = "nodes.khuslenkubernetes.com"
   launch_configuration = "${aws_launch_configuration.nodes-khuslenkubernetes-com.id}"
-  max_size             = 2
-  min_size             = 2
+  max_size             = "${var.node_max_size}"
+  min_size             = "${var.node_min_size}"
   vpc_zone_identifier  = ["${aws_subnet.us-west-2a-khuslenkubernetes-com.id}", "${aws_subnet.us-west-2b-khuslenkubernetes-com.id}", "${aws_subnet.us-west-2c-khuslenkubernetes-com.id}"]
 
   tag = {
